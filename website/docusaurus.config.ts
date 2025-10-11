981ee2c72532c49a65e1d022d0643f7c2f45bd79
wecoding <wecoding@yeah.net>
2025-10-11 10:47:13 +0800

    docs: update pages

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -28,7 +28,7 @@ const config: Config = {
     // deploymentBranch: 'main',
     trailingSlash: false,
 
-    onBrokenLinks: 'ignore',
+    onBrokenLinks: 'warn',
 
     // Even if you don't use internationalization, you can use this field to set
     // useful metadata like html lang. For example, if your site is Chinese, you
